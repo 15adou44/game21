87f49baceef494f2fe0efb390e4b6c26cb1e4d57
vortigaunted <82476383+vortigaunted@users.noreply.github.com>
2021-06-02 10:57:56 +1200

    aniamtiuon

--- spike/Player.gd
+++ spike/Player.gd
@@ -9,20 +9,25 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	motion.x = Input.get_action_strength("ui_right")- Input.get_action_strength("ui_left")
+	motion.x *= SPEED
+	if motion.x != 0:
+		#set run anim here
+		if motion.x > 0:
+			$Sprite.flip_h = false
+		else:
+			$Sprite.flip_h = true
 	
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-		$Sprite.flip_h = false
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-		$Sprite.flip_h = true
-	else:
-		motion.x = 0
+	if motion.x == 0:
+		pass
 		$AnimationPlayer.play("IdleRight")
+		
+		
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+			$Sprite.set_frame(2)
 	
 	motion = move_and_slide(motion, UP)
 	#print(motion)
