e83f3819ce270ae8415fc11f1b7951d44a8c1453
vortigaunted <82476383+vortigaunted@users.noreply.github.com>
2021-06-11 10:59:14 +1200

    deez

--- spike/WorldComplete.gd
+++ spike/WorldComplete.gd
@@ -5,4 +5,5 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			get.tree().change_scene(Screen2.tscn)
+			pass
+			#get.tree().change_scene(Screen2.tscn)
