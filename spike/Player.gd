021f966b960487ca8c3b1eff7102e38a7c666115
vortigaunted <82476383+vortigaunted@users.noreply.github.com>
2021-06-29 11:00:01 +1200

    dashing code

--- spike/Player.gd
+++ spike/Player.gd
@@ -22,7 +22,11 @@ func _physics_process(delta):
 		$AnimationPlayer.play("IdleRight")
 	if is_on_floor():if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-		
+	
+	motion = move_and_slide(motion, UP)
+	#print(motion)
+	pass
+
 #dash
 var dashDirection = Vector2(1, 0)
 var canDash = false
@@ -33,6 +37,7 @@ func dash():
 		canDash = true
 	
 	if Input.is_action_pressed("ui_right"):
+		pass
 		
 		
 		#if Input.is_action_pressed("ui_down"):
@@ -40,6 +45,3 @@ func dash():
 			#motion.x = 0
 			#motion.y = 0
 	
-	motion = move_and_slide(motion, UP)
-	#print(motion)
-	pass
